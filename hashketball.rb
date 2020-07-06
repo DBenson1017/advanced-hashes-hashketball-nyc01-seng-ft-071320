181afc725f4a2d5c1af832eb32e12fa1585a85e1
Devin Benson <devinbenson@gmail.com>
2020-07-06 01:05:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -216,7 +216,7 @@ def player_numbers(name)
     biggest_shoe = 0 
     rebounds = 0 
     game_hash.each do |team, team_value| 
-      team_value[:players]
+      team_value[:players].each do |name|
         if name[:shoe] > biggest_shoe 
           name[:shoe] = biggest_shoe
           rebounds = name[:rebounds]
